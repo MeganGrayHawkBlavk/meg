fe5d05c22c675839cc7a5814f98dc8aa0eff104c
Megan GrayHawk <megangrayhawkpro@gmail.com>
2024-11-09 15:48:40 -0700

    Update pizzacat.js
    changed front from green to purple.

--- pizzacat.js
+++ pizzacat.js
@@ -2,6 +2,6 @@ const greeting = document.getElementById("greeting");
 greeting.style.textAlign = "center";
 greeting.style.marginTop = "60px";
 greeting.style.fontSize = "50px";
-greeting.style.color = "#1E7421";
+greeting.style.color = "#e699f2";
 const username = window.prompt("what's your name?", "Pumpkin");
 greeting.innerHTML = "Welcome to my site, " + username + "!";
